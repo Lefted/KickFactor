9767f039b16613b7ba428397e571c22150440733
Gartn <Gartn@NilsPC.fritz.box>
2019-01-28 18:40:39 +0100

    Build 4
    - Added overloading of crop()
    	- Now uses grid counting for getting sprite sections aswell
    - Added width and height as constants to SpriteSheet.class

--- src/dev/lefted/graphics/SpriteSheet.java
+++ src/dev/lefted/graphics/SpriteSheet.java
@@ -6,15 +6,24 @@ public class SpriteSheet {
 
 	// VARIABLES
 	private BufferedImage sheet;
+	private final int WIDTH;
+	private final int HEIGHT;
 
 	// CONSTRUCTOR
-	public SpriteSheet(BufferedImage sheet) {
+	public SpriteSheet(BufferedImage sheet, int WIDTH, int HEIGHT) {
 		this.sheet = sheet;
+		this.WIDTH = WIDTH;
+		this.HEIGHT = HEIGHT;
 	}
 
-	// crop sheet to one section
+	// METHOD OVERLOADING
+	// crop sheet to one section using coordinates
 	public BufferedImage crop(int x, int y, int width, int height) {
 		return sheet.getSubimage(x, y, width, height);
 	}
 
+	// crop sheet to one section using grid counting
+	public BufferedImage crop(int x, int y) {
+		return sheet.getSubimage(x * WIDTH, y * HEIGHT, WIDTH, HEIGHT);
+	}
 }
